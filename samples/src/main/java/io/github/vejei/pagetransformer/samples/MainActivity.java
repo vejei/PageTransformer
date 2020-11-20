5de23ac6207bfbaa33840bba9d17fe88225d40e4
vejei <chengsnem@163.com>
2020-11-20 22:13:15 +0800

    Initialize project

---
+++ samples/src/main/java/io/github/vejei/pagetransformer/samples/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.vejei.pagetransformer.samples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
