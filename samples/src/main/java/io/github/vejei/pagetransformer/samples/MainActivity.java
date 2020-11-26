30b1adc6e56bec2dbd193fc18b17cae397532c7c
vejei <chengsnem@163.com>
2020-11-26 17:04:00 +0800

    Add buttons to open samples screen

--- samples/src/main/java/io/github/vejei/pagetransformer/samples/MainActivity.java
+++ samples/src/main/java/io/github/vejei/pagetransformer/samples/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.vejei.pagetransformer.samples;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_view_pager).setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, SamplesActivity.class);
+            intent.putExtra("sample_type", 0);
+            startActivity(intent);
+        });
+        findViewById(R.id.button_view_pager2).setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, SamplesActivity.class);
+            intent.putExtra("sample_type", 1);
+            startActivity(intent);
+        });
     }
 }
