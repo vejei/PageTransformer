51e0f6aac9f7f2985f1056ad8c03cc7f64c280fc
vejei <chengsnem@163.com>
2020-11-26 16:33:50 +0800

    transformers superclass

---
+++ pagetransformer/src/main/java/io/github/vejei/pagetransformer/PageTransformer.java
@@ -0,0 +1,8 @@
+package io.github.vejei.pagetransformer;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+public abstract class PageTransformer implements ViewPager.PageTransformer,
+        ViewPager2.PageTransformer {
+}
